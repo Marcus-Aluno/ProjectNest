080735d675def82351ca9f103759859dd2008152
Marcus Santos <marcus.c@aluno.ifsp.edu.br>
2024-05-01 00:11:47 -0300

    Criação de testes para instanciar entidades

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,16 @@ export class UserEntity {
     constructor(public readonly props: UserProps){
         this.props.CreateAt = this.props.CreateAt ?? new Date()
     }
+    get name(): string{
+        return this.props.name
+    }
+    get email(): string{
+        return this.props.email
+    }
+    get password(): string{
+        return this.props.password
+    }
+    get CreateAt(): Date {
+        return this.props.CreateAt
+    }
 }
